196692b375d87cb9e1f3a91d4e57ae069b701e79
Gram Smith <grantms4455@gmail.com>
2026-06-29 18:28:59 -0600

    Add caffeine

--- Install.sh
+++ Install.sh
@@ -1,6 +1,7 @@
 # Requires admins perms to run
 dnf copr enable v8v88v8v88/helium && dnf install helium # Install Browser
 dnf install R && dnf copr enable iucar/rstudio && dnf install rstudio-desktop # R and Rstudio
+dnf install gnome-shell-extension-caffeine # Install Caffeine
 flatpak install flathub com.super_productivity.SuperProductivity org.keepassxc.KeePassXC org.jellyfin.JellyfinDesktop org.kde.krita org.polymc.PolyMC com.github.Anuken.Mindustry # Various applications I like
 dnf install nvim 
 
