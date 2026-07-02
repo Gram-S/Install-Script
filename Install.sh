b28d2fc0ef0ae8b3434dbcbdadc41167c19b6d12
Gram Smith <grantms4455@gmail.com>
2026-07-02 02:00:05 -0600

    Update todo list

--- Install.sh
+++ Install.sh
@@ -1,10 +1,11 @@
 # Requires admins perms to run
 dnf copr enable v8v88v8v88/helium && dnf install helium # Install Browser
 dnf install R && dnf copr enable iucar/rstudio && dnf install rstudio-desktop # R and Rstudio
-dnf install gnome-shell-extension-caffeine # Install Caffeine
-flatpak install flathub com.super_productivity.SuperProductivity org.keepassxc.KeePassXC org.jellyfin.JellyfinDesktop org.kde.krita org.polymc.PolyMC com.github.Anuken.Mindustry # Various applications I like
+# dnf install gnome-shell-extension-caffeine # Install Caffeine
+flatpak install flathub io.github.alainm23.planify org.keepassxc.KeePassXC org.jellyfin.JellyfinDesktop org.kde.krita org.polymc.PolyMC com.github.Anuken.Mindustry # Various applications I like
 dnf install nvim 
 
 # My neovim config
 git clone https://github.com/Gram-S/dreamer-nvim
-mkdir .config/nvim && mv dreamer-nvim/* $_
+mkdir .config/nvim 
+mv dreamer-nvim/* $_
