6e6d5773e2728ed4d5040811ab3deaa1ff731534
Gram Smith <grantms4455@gmail.com>
2026-07-24 21:56:33 -0600

    Give the 2nd opition

--- crash_script.sh
+++ crash_script.sh
@@ -1,6 +1,16 @@
 # Script incase waydroid crashes
 
 sudo venv/bin/python3 main.py hack nodataperm # MUST be ran in waydroid_script directory from casualsnek's repo 
-sleep 5m
+sleep 2m
 waydroid session stop
 sudo venv/bin/python3 main.py remove nodataperm
+
+# THis should also work - but must be reran if another game is installed 
+#sudo waydroid shell
+#chmod 777 -R /sdcard/Android
+#chmod 777 -R /data/media/0/Android 
+#chmod 777 -R /sdcard/Android/data
+#chmod 777 -R /data/media/0/Android/obb 
+#chmod 777 -R /mnt/*/*/*/*/Android/data
+#chmod 777 -R /mnt/*/*/*/*/Android/obb
+#exit
