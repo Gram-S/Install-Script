e5624719b4d55e63137d83156c0edda26e4aeab4
Gram Smith <grantms4455@gmail.com>
2026-07-24 21:48:02 -0600

    Create crash_script.sh

---
+++ crash_script.sh
@@ -0,0 +1,4 @@
+sudo venv/bin/python3 main.py hack nodataperm # MUST be ran in waydroid_script directory from casualsnek's repo 
+sleep 5m
+waydroid session stop
+sudo python main.py remove nodataperm 
