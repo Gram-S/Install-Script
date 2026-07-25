85a0601855ff547a372c4766dd74520d009a6023
Gram Smith <grantms4455@gmail.com>
2026-07-24 21:58:44 -0600

    Update admin perms

--- Install.sh
+++ Install.sh
@@ -1,15 +1,13 @@
 # Requires admins perms to run
-sudo -i
-dnf copr enable v8v88v8v88/helium && dnf install helium # Install Browser
-dnf install R && dnf copr enable iucar/rstudio && dnf install rstudio-desktop # R and Rstudio
+sudo dnf copr enable v8v88v8v88/helium && dnf install helium # Install Browser
+sudo dnf install R && dnf copr enable iucar/rstudio && dnf install rstudio-desktop # R and Rstudio
 # dnf install gnome-shell-extension-caffeine # Install Caffeine
-flatpak install flathub io.github.alainm23.planify org.keepassxc.KeePassXC org.jellyfin.JellyfinDesktop org.kde.krita org.polymc.PolyMC com.github.Anuken.Mindustry # Various applications I like
-dnf install nvim 
+sudo flatpak install flathub io.github.alainm23.planify org.keepassxc.KeePassXC org.jellyfin.JellyfinDesktop org.kde.krita org.polymc.PolyMC com.github.Anuken.Mindustry # Various applications I like
+sudo dnf install nvim 
 
 # Waydroid 
-waydroid init -s GAPPS # Need system OTA and Vendor OTA urls
-waydroid shell -- sh -c "sqlite3 /data/data/*/*/gservices.db 'select value from main where name = \"android_id\";'" # Put in https://www.google.com/android/uncertified
-exit # Leave root shell
+sudo waydroid init -s GAPPS # Need system OTA and Vendor OTA urls
+sudo waydroid shell -- sh -c "sqlite3 /data/data/*/*/gservices.db 'select value from main where name = \"android_id\";'" # Put in https://www.google.com/android/uncertified
 
 # My neovim config
 git clone https://github.com/Gram-S/dreamer-nvim
