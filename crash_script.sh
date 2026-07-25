348e34c7b9244f01aa0d76ba67ab6ca7d077ea41
Gram Smith <grantms4455@gmail.com>
2026-07-24 21:50:24 -0600

    Fix removal line

--- crash_script.sh
+++ crash_script.sh
@@ -3,4 +3,4 @@
 sudo venv/bin/python3 main.py hack nodataperm # MUST be ran in waydroid_script directory from casualsnek's repo 
 sleep 5m
 waydroid session stop
-sudo python main.py remove nodataperm 
+sudo venv/bin/python3 main.py remove nodataperm
